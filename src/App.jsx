bbd75cfd8f51614e79093391119c9d2365bbb8e8
sjanavi69 <148612017+sjanavi69@users.noreply.github.com>
2025-05-28 17:51:23 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import './Module 3/Introduction to ReactJS/Ques_3';
-import Greeting from './Module 3/Introduction to ReactJS/Ques_3';
+import './Module 3/Introduction to ReactJS/Ques_5';
+import Profile from './Module 3/Introduction to ReactJS/Ques_5';
+
 
 function App() {
-
   return (
     <div>
-   
-      <Greeting />
+      <h1>Welcome to the App</h1>
+      <Profile name="Janavi" age={22} />
     </div>
   );
-  
 }
 
 export default App;
--- src/Module 3/Introduction to ReactJS/Ques_5.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_5.jsx
@@ -3,3 +3,15 @@
 
 // Step 1: Write your code within the file, by the name of component as Profile
 // Step 2: Update App.js to pass props to the Profile component
+import React from 'react';
+
+function Profile(props) {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <p>Age: {props.age}</p>
+    </div>
+  );
+}
+
+export default Profile;
