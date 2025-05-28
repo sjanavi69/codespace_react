8c9a729db6cc386688b83d56d4506faee4faba15
sjanavi69 <148612017+sjanavi69@users.noreply.github.com>
2025-05-28 17:39:44 +0000

    updated

--- src/Module 3/Introduction to ReactJS/Ques_4.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_4.jsx
@@ -5,7 +5,7 @@ function Error() {
     return (
         <div>
             <h1>Unclosed tag</h1>
-            <p>Self-closing tag example <img src="example.png"></img></p>
+            <p>Self-closing tag example <img src="example.png"/></p>
             <ul>
                 <li>List item 1</li>
                 <li>List item 2</li>
