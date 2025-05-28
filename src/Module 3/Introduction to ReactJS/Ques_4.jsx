c10de6c050374069722bbcb8748ff5fb16c98aeb
sjanavi69 <148612017+sjanavi69@users.noreply.github.com>
2025-05-28 17:36:49 +0000

    updated

--- src/Module 3/Introduction to ReactJS/Ques_4.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_4.jsx
@@ -4,11 +4,11 @@
 function Error() {
     return (
         <div>
-            <h1>Unclosed tag
+            <h1>Unclosed tag</h1>
             <p>Self-closing tag example <img src="example.png"></img></p>
             <ul>
-                <li>List item 1
-                <li>List item 2
+                <li>List item 1</li>
+                <li>List item 2</li>
             </ul>
         </div>
     );
